3816cf96359ac74e1c93aff01378ff3615e8b4ef
Roberto Castillo <38573736+roberteban@users.noreply.github.com>
2025-07-08 21:14:09 -0400

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -10,7 +9,6 @@ import { ApiInterceptor } from './app/core/interceptors/api.interceptor';
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
-    provideRouter([]),
     importProvidersFrom(ReactiveFormsModule),
     {
       provide: HTTP_INTERCEPTORS,
